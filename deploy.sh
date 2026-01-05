0764158e51d09cf121daeb68f6804450b1ad02e9
Ishan Shrestha <ishanshrestha941@gmail.com>
2026-01-05 11:59:32 +0545

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,13 @@
-#!/bin/bash
+CONTAINER_NAME=go-docker-app
 
 echo "Stopping old container if exists..."
-docker stop go-docker_app || true
-docker rm go-docker_app || true
-docker rmi go-docker_app:latest || true
+docker stop $CONTAINER_NAME || true
+docker rm $CONTAINER_NAME || true
 
-
-echo "Building new Docker image..."
-docker build -t go-docker-app:latest .
-
+echo "Building Docker image..."
+docker build -t $CONTAINER_NAME:latest .
 
 echo "Running new container..."
-docker run -d --name go-docker_app -p 8080:8080 go-docker-app:latest
+docker run -d -p 8080:8080 --name $CONTAINER_NAME $CONTAINER_NAME:latest
 
-echo "Deployment complete. Application is running on port 8080."
+echo "Deployment complete!"
